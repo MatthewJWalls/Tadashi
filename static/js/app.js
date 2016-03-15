4d2058eb20c613303c85d36d7f6d67c7bf83f0a4
Matthew Walls <matthewjwalls@googlemail.com>
2016-03-15 20:42:18 +0000

    Re-introduce Angular routes

--- static/js/app.js
+++ static/js/app.js
@@ -2,15 +2,31 @@ require("./lib/angular.min.js")
 require("./lib/angular-sanitize.min.js")
 require("./lib/angular-animate.min.js")
 require("./lib/angular-animate.min.js")
+require("./lib/angular-route.min.js")
 require("./lib/angular-mocks.js")
 
 var particles = require("./particles")
 var ime = require("./ime")
 
 var app = angular.module(
-    "quizzer", ["ngAnimate", "ngSanitize"]
+    "quizzer", ["ngAnimate", "ngSanitize", "ngRoute"]
 );
 
+var config = function($interpolateProvider, $locationProvider, $routeProvider) {
+
+    $interpolateProvider.startSymbol('{$');
+    $interpolateProvider.endSymbol('$}');
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when("/particles", {
+        templateUrl : "/static/partials/quiz.html",
+        controller : "QuizController",
+        controllerAs : "quiz"
+    });
+
+}
+
+app.config(config)
 app.service("Progression", particles.Progression);
 app.controller("QuizController", particles.QuizController);
 app.factory("ParticleService", particles.ParticleService);
